f643597064b0189b3d645da829126544c9d1a3f2
Krjnstore <amunsura200@gmail.com>
2025-11-26 06:50:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
-IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
-AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
+API_ID = int(os.environ.get("API_ID", "25527780"))
+API_HASH = os.environ.get("API_HASH", "1ed74b5498913cd53b47063f692abd38")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID", "-1002557949787")
+IS_PRIVATE = os.environ.get("IS_PRIVATE", True) # any input is ok But True preferable
+OWNER_ID = int(os.environ.get("OWNER_ID", "8124792926"))
+UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '-1002534798811')
+AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "8124792926").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
